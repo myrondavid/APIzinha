dc9a04446fcefa4d6de0acae5a2145d9b50fc538
Myron <myrondavid96@gmail.com>
2018-03-07 23:01:23 -0300

    API de teste

---
+++ APIzinha/Controllers/MessagesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIzinha.Models;
+
+namespace APIzinha.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Messages")]
+    public class MessagesController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public MessagesController(DatabaseContext context)
+        {
+            _context = context;
+            if(_context.Messages.Count() == 0)
+            {
+                _context.Messages.Add(new Message { Text = "First Message", Author = "Myron" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Messages
+        [HttpGet]
+        public IEnumerable<Message> GetMessages()
+        {
+            return _context.Messages;
+        }
+
+        // GET: api/Messages/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMessage([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var message = await _context.Messages.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(message);
+        }
+
+        // PUT: api/Messages/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMessage([FromRoute] long id, [FromBody] Message message)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != message.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(message).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MessageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Messages
+        [HttpPost]
+        public async Task<IActionResult> PostMessage([FromBody] Message message)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMessage", new { id = message.Id }, message);
+        }
+
+        // DELETE: api/Messages/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMessage([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var message = await _context.Messages.SingleOrDefaultAsync(m => m.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            _context.Messages.Remove(message);
+            await _context.SaveChangesAsync();
+
+            return Ok(message);
+        }
+
+        private bool MessageExists(long id)
+        {
+            return _context.Messages.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ APIzinha/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIzinha.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
